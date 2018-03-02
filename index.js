b69d5b6f4b06462749895115452e71df1100ce11
Carlos <carlos.jmt93@gmail.com>
2018-03-02 22:24:00 +0000

    Servidor express F02

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+var express = require("express")
+var cool = require("cool-ascii-faces"); //nos permite invocarla como función
+var app = express();
+
+app.get("/hello",(req,res)=>{
+    
+    res.send("hello!");
+});
+
+app.listen(process.env.PORT);
+//console.log("Hello world!");
+console.log(cool())
