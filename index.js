8d23bd72458bd456c085bb7c4cd99f148b576087
Carlos <carlos.jmt93@gmail.com>
2018-03-12 10:04:30 +0000

    merge solved

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-var express = require ("express");
-var app  = express();
-var port = (process.env.PORT || 1607);
 
-
-app.use("/", express.static(__dirname+"/public"))
-
-
-app.listen(port,()=>{
-    console.log("server ready on port " + port + "!");
-}).on("error",(e)=>{
-    console.log("SERVER NOT READY: "+e);
-});
-
-console.log("server setting up... ");
-=======
 var express = require("express");
 var path = require("path");
 
@@ -34,4 +18,4 @@ app.listen(port, ()=>{
 }).on("error", (e)=>{
     console.log("Server NOT READY: "+e);
 });
->>>>>>> 17ecc3cb96ac15270a43bffddcca42bd62390303
+
