94549538a1c8b7fd67864c008efe2bbd08758632
Carlos <carlos.jmt93@gmail.com>
2018-03-12 09:47:01 +0000

    merge

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require("express")
 var cool = require("cool-ascii-faces"); //nos permite invocarla como función
 var app = express();
 
+app.use("/",express.static(__dirname+"/public"));
+
 app.get("/hello",(req,res)=>{
     
     res.send("hello!");
