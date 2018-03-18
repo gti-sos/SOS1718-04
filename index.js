05eb5e44d9edfde803039b620f7aa6bab43d08be
sos171804arg <rgfandres@gmail.com>
2018-03-18 17:44:47 +0000

    api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var DataStore = require("nedb");
+//var DataStore = require("nedb");
 var path = require("path");
 
 var port = (process.env.PORT || 1607);
@@ -256,6 +256,175 @@ app.put(BASE_API_PATH+"/unemployment-rates/:province",(req,res)=>{
 
 
 //################### Fin API REST de Cristian:
+
+//################### Inicio API REST de Andrés:
+
+var initialGraduationRates = [
+        { 
+            "town" :"huelva", 
+            "year" : 2015, 
+            "public school" :79.4 , 
+            "private school" : 100.0 , 
+            "charter school" :83.9 
+        },
+        { 
+           "town": "seville", 
+           "year": 2015, 
+           "public school" :80.9 , 
+           "private school":98.2 ,
+           "charter school" :89.5
+        }
+    ];
+
+// Descomentar en caso de hacer persistencia
+// var db = new DataStore({
+//     filename: dbFileName,
+//     autoload: true
+// });
+
+// db.find({},(err,graduationRates)=>{
+//     if(err){
+//         console.error(" Error accesing DB");
+//         process.exit(1);
+//     }
+    
+//     if(graduationRates.length == 0){
+//         console.log("Empty DB");
+//         db.insert(initualGraduationRates);
+//     }else{
+//         console.log("DB initialized with "+graduationRates.length+" graduation-rates");
+//     }
+    
+// });
+
+app.get(BASE_API_PATH+"/graduationRates-rates/loadInitialData",(req,res)=>{
+    console.log(Date() + " - GET /graduationRates-rates/loadInitialData");
+    if(initialGraduationRates.length == 0){
+        initialGraduationRates = [
+        { 
+            "town" :"huelva", 
+            "year" : 2015, 
+            "public school" :79.4 , 
+            "private school" : 100.0 , 
+            "charter school" :83.9 
+        },
+        { 
+           "town": "seville", 
+           "year": 2015, 
+           "public school" :80.9 , 
+           "private school":98.2 ,
+           "charter school" :89.5
+        }
+        ];
+    }
+    //Inicializamos los datos en caso de necesitarlo
+    // db.find({},(err,gruaduationRates)=>{
+    //     if(err){
+    //         console.error(" Error accesing DB");
+    //         process.exit(1);
+    //     }
+        
+    //     if(graduationRates.length == 0){
+    //         console.log("Empty DB");
+    //         db.insert(initialGraduationRates);
+    //     }else{
+    //         console.log("DB initialized with "+graduationRates.length+" graduation-ates");
+    //     }
+        
+    // });
+    res.sendStatus(200);
+});
+
+app.get(BASE_API_PATH+"/graduation-rates",(req,res)=>{
+    console.log(Date() + " - GET /graduation-rates");
+//     db.find({},(err,graduationRates)=>{
+//      if(err){
+//          console.error(" Error accesing DB");
+//          res.sendStatus(500);
+//          return;
+//     }
+//     res.send(graduationRates);
+// });
+    res.send(initialGraduationRates);
+});
+
+app.post(BASE_API_PATH+"/graduation-rates",(req,res)=>{
+    console.log(Date() + " - POST /graduation-rates");
+    var data = req.body;
+    initialGraduationRates.push(data);
+    res.sendStatus(201);
+});
+
+//Al hacer un put a un recurso no concreto envía un código de error
+app.put(BASE_API_PATH+"/graduation-rates",(req,res)=>{
+    console.log(Date() + " - PUT /graduation-rates");
+    res.sendStatus(405);
+});
+
+app.delete(BASE_API_PATH+"/graduationR-rates",(req,res)=>{
+    console.log(Date() + " - DELETE /graduation-rates");
+    initialGraduationRates = [];
+    
+    //db.remove({});
+    
+    res.sendStatus(200);
+});
+
+//Recursos concretos
+app.get(BASE_API_PATH+"/graduation-rates/:province",(req,res)=>{
+    var province = req.params.province;
+    console.log(Date() + " - GET /graduation-rates/"+province);
+    res.send(initialGraduationRates.filter((c)=>{
+        return (c.province == province);
+    })[0]);
+});
+
+app.delete(BASE_API_PATH+"/graduation-rates/:province",(req,res)=>{
+    var province = req.params.province;
+    console.log(Date() + " - DELETE /graduation-rates/"+province);
+    initialGraduationRates = initialGraduationRates.filter((c)=>{
+        return (c.province != province);
+    });
+    res.sendStatus(200);
+});
+
+app.post(BASE_API_PATH+"/graduation-rates/:province",(req,res)=>{
+    var province = req.params.province;
+    console.log(Date() + " - POST /graduation-rates/"+province);
+    res.sendStatus(405);
+});
+
+app.put(BASE_API_PATH+"/graduation-rates/:province",(req,res)=>{
+    var province = req.params.province;
+    var data = req.body;
+    console.log(Date() + " - PUT /graduation-rates/"+province);
+    
+    //db.update({"name":contact.name},contact,(err,numUpdate)=>{
+    //    console.log("Update: "+numUpdate);
+    //});
+    //Comprobamos si hay incongruencias en los datos antes de actuar
+    if(province != data.province){
+        res.sendStatus(409);
+        return;
+    }
+    
+    initialGraduationRates = initialGraduationRates.map((c)=>{
+        console.log("entra");
+        if(c.province == data.province){
+            res.sendStatus(200);
+            return data;
+        }else{
+            res.sendStatus(200);
+            return c;
+        }
+    });
+});
+
+
+//################### Fin API REST de Andrés:
+
+
+
 
 
 app.listen(port, ()=>{
