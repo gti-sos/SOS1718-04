0b4403c25f2e65b1ed1a990c9bff508b2a1cdec6
Carlos <carlos.jmt93@gmail.com>
2018-03-18 21:17:22 +0000

    metodos de la API carlos

--- index.js
+++ index.js
@@ -518,6 +518,73 @@ app.post(BASE_API_PATH + "/medical-attention-according-to-type-rates", (req, res
 });
 
 
+//Al hacer un put a un recurso no concreto envía un código de error
+app.put(BASE_API_PATH + "/medical-attention-according-to-type-rates", (req, res) => {
+    console.log(Date() + " - PUT /medical-attention-according-to-type-rates");
+    res.sendStatus(405);
+});
+
+app.delete(BASE_API_PATH + "/medical-attention-according-to-type-rates", (req, res) => {
+    console.log(Date() + " - DELETE /medical-attention-according-to-type-rates");
+    initialMedicalAttentionAccordingtoTypeRates = [];
+
+    //db.remove({});
+
+    res.sendStatus(200);
+});
+
+//Recursos concretos
+
+app.get(BASE_API_PATH + "/medical-attention-according-to-type-rates/:province", (req, res) => {
+    var province = req.params.province;
+    console.log(Date() + " - GET /medical-attention-according-to-type-rates/" + province);
+    res.send(initialMedicalAttentionAccordingtoTypeRates.filter((c) => {
+        return (c.province == province);
+    })[0]);
+});
+
+app.delete(BASE_API_PATH + "/medical-attention-according-to-type-rates/:province", (req, res) => {
+    var province = req.params.province;
+    console.log(Date() + " - DELETE /medical-attention-according-to-type-rates/" + province);
+    initialMedicalAttentionAccordingtoTypeRates = initialMedicalAttentionAccordingtoTypeRates.filter((c) => {
+        return (c.province != province);
+    });
+    res.sendStatus(200);
+});
+
+app.post(BASE_API_PATH + "/graduation-rates/:province", (req, res) => {
+    var province = req.params.province;
+    console.log(Date() + " - POST /graduation-rates/" + province);
+    res.sendStatus(405);
+});
+
+app.put(BASE_API_PATH + "/graduation-rates/:province", (req, res) => {
+    var province = req.params.province;
+    var data = req.body;
+    console.log(Date() + " - PUT /graduation-rates/" + province);
+
+    //db.update({"name":contact.name},contact,(err,numUpdate)=>{
+    //    console.log("Update: "+numUpdate);
+    //});
+    //Comprobamos si hay incongruencias en los datos antes de actuar
+    if (province != data.province) {
+        res.sendStatus(409);
+        return;
+    }
+
+    initialGraduationRates = initialGraduationRates.map((c) => {
+        console.log("entra");
+        if (c.province == data.province) {
+            res.sendStatus(200);
+            return data;
+        }
+        else {
+            res.sendStatus(200);
+            return c;
+        }
+    });
+});
+
 //################### Fin API REST de Carlos:
 
 
