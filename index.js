524dc5f16639fcf191a06240e17755880c101c4d
sos171804crp <crirompov@alum.us.es>
2018-03-18 16:27:12 +0000

    API REST de Cristian asociado a la issue #12

--- index.js
+++ index.js
@@ -1,14 +1,236 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+//var DataStore = require("nedb");
 var path = require("path");
 
+var port = (process.env.PORT || 1607);
+var BASE_API_PATH = "/api/v1";
+// Descomentar en caso de hacer persistencia
+//var dbFileName = __dirname+"/contacts.db";
+
 var app = express();
-var port = ( process.env.PORT || 16778 );
+
+app.use(bodyParser.json());
 
 app.use("/", express.static(path.join(__dirname+"/public")));
 
-app.get("/hello",(req,res)=>{
-    res.send("Hello world!");
+//Debería ir pero no entiendo por que no funciona
+app.get(BASE_API_PATH+"/unemployment-rates/help",(res,req)=>{
+    return res.redirect('https://documenter.getpostman.com/view/3896692/sos1718-04-unemployment-rates-v1/RVnZgdXZ');
 });
+
+//################### Inicio API REST de Cristian:
+
+var unemploymentRates = [
+        { 
+            "province" : "sevilla",
+            "year" : 1981,
+            "illiterate" : 3.7,
+            "first-grade" : 5.1,
+            "second-grade" : 24.9,
+            "third-degree" : 0.1,
+            "min-age" : 16,
+            "max-age" : 19
+        }, 
+        { 
+            "province" : "malaga",
+            "year" : 1981,
+            "illiterate" : 2.5,
+            "first-grade" : 3.0,
+            "second-grade" : 16.9,
+            "third-degree" : 0.1,
+            "min-age" : 16,
+            "max-age" : 19
+        },
+         { 
+            "province" : "cadiz",
+            "year" : 1981,
+            "illiterate" : 2.7,
+            "first-grade" : 4.3,
+            "second-grade" : 13.4,
+            "third-degree" : 0.0,
+            "min-age" : 16,
+            "max-age" : 19
+        },
+         { 
+            "province" : "almeria",
+            "year" : 1981,
+            "illiterate" : 0.5,
+            "first-grade" : 0.8,
+            "second-grade" : 3.3,
+            "third-degree" : 0,
+            "min-age" : 16,
+            "max-age" : 19
+        },
+         { 
+            "province" : "cordoba",
+            "year" : 1981,
+            "illiterate" : 1.7,
+            "first-grade" : 2.7,
+            "second-grade" : 11.9,
+            "third-degree" : 0,
+            "min-age" : 16,
+            "max-age" : 19
+        },
+    ];
+
+// Descomentar en caso de hacer persistencia
+// var db = new DataStore({
+//     filename: dbFileName,
+//     autoload: true
+// });
+
+app.get(BASE_API_PATH+"/unemployment-rates/loadInitialData",(req,res)=>{
+    console.log(Date() + " - GET /unemployment-rates/loadInitialData");
+    // db.find({},(err,contacts)=>{
+    // if(err){
+    //     console.error(" Error accesing DB");
+    //     res.sendStatus(500);
+    //     return;
+    // }
+    if(unemploymentRates.length == 0){
+        unemploymentRates = [
+        { 
+            "province" : "sevilla",
+            "year" : 1981,
+            "illiterate" : 3.7,
+            "first-grade" : 5.1,
+            "second-grade" : 24.9,
+            "third-degree" : 0.1,
+            "min-age" : 16,
+            "max-age" : 19
+        }, 
+        { 
+            "province" : "malaga",
+            "year" : 1981,
+            "illiterate" : 2.5,
+            "first-grade" : 3.0,
+            "second-grade" : 16.9,
+            "third-degree" : 0.1,
+            "min-age" : 16,
+            "max-age" : 19
+        },
+         { 
+            "province" : "cadiz",
+            "year" : 1981,
+            "illiterate" : 2.7,
+            "first-grade" : 4.3,
+            "second-grade" : 13.4,
+            "third-degree" : 0.0,
+            "min-age" : 16,
+            "max-age" : 19
+        },
+         { 
+            "province" : "almeria",
+            "year" : 1981,
+            "illiterate" : 0.5,
+            "first-grade" : 0.8,
+            "second-grade" : 3.3,
+            "third-degree" : 0,
+            "min-age" : 16,
+            "max-age" : 19
+        },
+         { 
+            "province" : "cordoba",
+            "year" : 1981,
+            "illiterate" : 1.7,
+            "first-grade" : 2.7,
+            "second-grade" : 11.9,
+            "third-degree" : 0,
+            "min-age" : 16,
+            "max-age" : 19
+        },
+        ];
+    }
+    res.sendStatus(200);
+});
+
+app.get(BASE_API_PATH+"/unemployment-rates",(req,res)=>{
+    console.log(Date() + " - GET /unemployment-rates");
+    // db.find({},(err,contacts)=>{
+    // if(err){
+    //     console.error(" Error accesing DB");
+    //     res.sendStatus(500);
+    //     return;
+    // }
+    res.send(unemploymentRates);
+});
+
+app.post(BASE_API_PATH+"/unemployment-rates",(req,res)=>{
+    console.log(Date() + " - POST /unemployment-rates");
+    var data = req.body;
+    unemploymentRates.push(data);
+    res.sendStatus(201);
+});
+
+//Al hacer un put a un recurso no concreto envía un código de error
+app.put(BASE_API_PATH+"/unemployment-rates",(req,res)=>{
+    console.log(Date() + " - PUT /unemployment-rates");
+    res.sendStatus(405);
+});
+
+app.delete(BASE_API_PATH+"/unemployment-rates",(req,res)=>{
+    console.log(Date() + " - DELETE /unemployment-rates");
+    unemploymentRates = [];
+    
+    //db.remove({});
+    
+    res.sendStatus(200);
+});
+
+//Recursos concretos
+app.get(BASE_API_PATH+"/unemployment-rates/:province",(req,res)=>{
+    var province = req.params.province;
+    console.log(Date() + " - GET /unemployment-rates/"+province);
+    res.send(unemploymentRates.filter((c)=>{
+        return (c.province == province);
+    })[0]);
+});
+
+app.delete(BASE_API_PATH+"/unemployment-rates/:province",(req,res)=>{
+    var province = req.params.province;
+    console.log(Date() + " - DELETE /unemployment-rates/"+province);
+    unemploymentRates = unemploymentRates.filter((c)=>{
+        return (c.province != province);
+    });
+    res.sendStatus(200);
+});
+
+app.post(BASE_API_PATH+"/unemployment-rates/:province",(req,res)=>{
+    var province = req.params.province;
+    console.log(Date() + " - POST /unemployment-rates/"+province);
+    res.sendStatus(405);
+});
+
+app.put(BASE_API_PATH+"/unemployment-rates/:province",(req,res)=>{
+    var province = req.params.province;
+    var data = req.body;
+    console.log(Date() + " - PUT /unemployment-rates/"+province);
+    
+    //db.update({"name":contact.name},contact,(err,numUpdate)=>{
+    //    console.log("Update: "+numUpdate);
+    //});
+    //Comprobamos si hay incongruencias en los datos antes de actuar
+    if(province != data.province){
+        res.sendStatus(409);
+        return;
+    }
+    
+    unemploymentRates = unemploymentRates.map((c)=>{
+        console.log("entra");
+        if(c.province == data.province){
+            res.sendStatus(200);
+            return data;
+        }else{
+            res.sendStatus(200);
+            return c;
+        }
+    });
+});
+
+
+//################### Fin API REST de Cristian:
+
 
 app.listen(port, ()=>{
     console.log("Server ready on port "+port+"!");
