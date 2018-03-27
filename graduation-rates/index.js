676be2cd190190718592c84dc6a334d0b26e5856
sos171804arg <rgfandres@gmail.com>
2018-03-27 23:15:05 +0000

    api

--- graduation-rates/index.js
+++ graduation-rates/index.js
@@ -127,7 +127,7 @@ app.get(BASE_API_PATH + "/graduation-rates", (req, res) => {
         console.log(Date() + " - POST /graduation-rates");
         var data = req.body;
         //Comprobamos si hay incongruencias en los datos antes de actuar
-        if (data.length > 5 || !data.hasOwnProperty("province") || !data.hasOwnProperty("year") ||
+        if (data.length > 6 || !data.hasOwnProperty("province") || !data.hasOwnProperty("year") ||
             !data.hasOwnProperty("public-school") || !data.hasOwnProperty("private-school") || !data.hasOwnProperty("charter-school"))
             {
             res.sendStatus(400);
@@ -208,14 +208,14 @@ app.put(BASE_API_PATH + "/graduation-rates/:province", (req, res) => {
         console.log(Date() + " - PUT /graduation-rates/" + province);
 
         //Comprobamos si hay incongruencias en los datos antes de actuar
-        if (province != data.province || data.length > 5 ||  !data.hasOwnProperty("province") || !data.hasOwnProperty("year")  ||
+        if (province != data.province || data.length > 6 ||  !data.hasOwnProperty("province") || !data.hasOwnProperty("year")  ||
             !data.hasOwnProperty("public-school") || !data.hasOwnProperty("private-school") || !data.hasOwnProperty("charter-school"))
              {
             res.sendStatus(400);
             return;
         }
         db.update({ "province": data.province }, data, (err, numUpdated) => {
-            console.log("Udapted: " + numUpdated);
+            console.log("Updated: " + numUpdated);
         });
 
         // initialUnemploymentRates = initialUnemploymentRates.map((c) => {
