5af9daa7b03ff071f0c91856b86c372f9597d635
sos171804arg <rgfandres@gmail.com>
2018-03-29 13:38:42 +0000

    enlaces heroku

--- graduation-rates/index.js
+++ graduation-rates/index.js
@@ -95,7 +95,7 @@ app.delete(BASE_API_PATH+"/graduation-rates",(req,res)=>{
                 res.sendStatus(404);
                 return;
             }
-            if ( Object.keys(datas).length!=5 ) {
+            if ( Object.keys(datas).length!=6 ) {
                 res.sendStatus(400);
                 return;
             }
