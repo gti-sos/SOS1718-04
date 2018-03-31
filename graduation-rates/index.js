6a75967db42ae7d66125abb40f7274697cdf5d90
sos171804arg <rgfandres@gmail.com>
2018-03-31 18:25:03 +0000

    .

--- graduation-rates/index.js
+++ graduation-rates/index.js
@@ -90,10 +90,10 @@ app.delete(BASE_API_PATH+"/graduation-rates",(req,res)=>{
                 res.sendStatus(404);
                 return;
             }
-            /*if ( datas[0].length>5) {
+            if ( datas[0].length>5) {
                 res.sendStatus(400);
                 return;
-            }*/
+            }
             res.send(datas.map((c) => {
                 delete c._id; //Quitamos el campo id
                 return c;
