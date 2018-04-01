4562c6c2dda4b729856e9b6257300fc9ab20f90e
Carlos <carlos.jmt93@gmail.com>
2018-04-01 15:22:01 +0000

    carlos tabla azul y errores

--- medical-attention-rates/index.js
+++ medical-attention-rates/index.js
@@ -26,11 +26,15 @@ medicalAttentionRates.register = function(app, db) {
         });
     });
 
-    //POST a recurso general
+    //POST a recurso general (HECHO)
     //POST to a general collection
     app.post(BASE_API_PATH + "/medical-attention-rates", (req, res) => {
         console.log(Date() + " - POST /medical-attention-rates");
         var data = req.body;
+        var province = req.body.province;
+        var year = req.body.year;
+        console.log("provincia input : " + province + ", year input : " + year);
+
 
         //comprobamos si el dato que se va a introducir contiene algún error, tamañi y nombre de las propiedades
         //we have to validate the data that we insert in the database, if exist some error we send a error message.
@@ -39,26 +43,43 @@ medicalAttentionRates.register = function(app, db) {
             res.sendStatus(400);
             return;
         }
+        db.find({ "province": province, "year": year }).toArray((err, medicalAttentionRates) => {
+            if (err) {
+                console.error("Error accesing DB");
+                res.sendStatus(500);
+                return;
+            }
+            //en caso de que no haya errores , insertamos el dato a la base de datos
+            //if there is not any error, the data will be inserted in the database
 
-        //en caso de que no haya errores , insertamos el dato a la base de datos
-        //if there is not any error, the data will be inserted in the database
-        db.insertOne(data, (err, numUpdated) => {
-            console.log("Insert: " + numUpdated);
+
+
+            if (medicalAttentionRates.length == 0) { //esta condicion es si ya esta creado un objeto con esos datos
+                db.insertOne(data, (err, numUpdated) => {
+                    console.log("Insert: " + numUpdated);
+                });
+                res.sendStatus(201);
+            }
+            console.log("Error the object was created before.");
+            res.sendStatus(409);
+
         });
-        res.sendStatus(201);
+
+
+
     });
 
 
 
     //PUT a un recurso general, no debe ser posible
-    //PUT to a general collection, it must not be possible
+    //PUT to a general collection, it must not be possible (HECHO)
     app.put(BASE_API_PATH + "/medical-attention-rates", (req, res) => {
         console.log(Date() + " - PUT /medical-attention-rates");
         res.sendStatus(405);
     });
 
 
-    //DELETE a un recurso general, borra todo el contenido
+    //DELETE a un recurso general, borra todo el contenido (HECHO)
     //DELETE to a general collection, it must delete the collection's objects
     app.delete(BASE_API_PATH + "/medical-attention-rates", (req, res) => {
         console.log(Date() + " - DELETE /medical-attention-rates");
@@ -69,11 +90,30 @@ medicalAttentionRates.register = function(app, db) {
         res.sendStatus(200);
     });
 
-    //GET a tun recurso concreto
-    //ESte no me funciona
+    //GET a un recurso concreto(HECHO)
+    app.get(BASE_API_PATH + "/medical-attention-rates/:province/:year", (req, res) => {
+        var province = req.params.province;
+        var year = req.params.year;
+        // console.log(province + "testeando");
+        console.log(Date() + " - GET /medical-attention-rates/" + province + "/"+year);
+
+
+
+        db.find({}).toArray((err, medicalAttentionRates) => {
+            if (err) {
+                console.error("Error accesing DB");
+                res.sendStatus(500);
+                return;
+            }
+            //console.log(medicalAttentionRates);
+            res.send(medicalAttentionRates.filter(c => c.province == province & c.year == year));
+        });
+    });
+
+
     app.get(BASE_API_PATH + "/medical-attention-rates/:province", (req, res) => {
         var province = req.params.province;
-        console.log(province + "testeando");
+        // console.log(province + "testeando");
         console.log(Date() + " - GET /medical-attention-rates/" + province);
 
 
@@ -84,18 +124,42 @@ medicalAttentionRates.register = function(app, db) {
                 res.sendStatus(500);
                 return;
             }
-            res.send(medicalAttentionRates).filter((c)=> {
-                return(c.province == province);
-            });
-            
+            //console.log(medicalAttentionRates);
+            res.send(medicalAttentionRates.filter(c => c.province == province));
         });
     });
-    
+
+
     //DELETE a un recurso concreto
-     app.delete(BASE_API_PATH + "/medical-attention-rates/:province", (req, res) => {
+    //no funciona bien 
+    app.delete(BASE_API_PATH + "/medical-attention-rates/:province/:year", (req, res) => {
         var province = req.params.province;
-        console.log(Date() + " - DELETE /medical-attention-rates/" + province);
-        db.remove({ "province": province });
+        var year = req.params.year;
+        console.log(Date() + " - DELETE /medical-attention-rates/" + province + "/" + year);
+        db.remove({ "province": province, "year": year });
+        //No borra ninguno , si coloco solo provincia funciona pero borra todos , y no es el objetivo en mi opinion
+        res.sendStatus(200);
+    });
+
+    //PUT a un recurso concreto (HECHO)
+    app.put(BASE_API_PATH + "/medical-attention-rates/:province/:year", (req, res) => {
+        var province = req.params.province;
+        var year = req.params.year;
+        var medicalAttentionRate = req.body;
+
+        console.log(Date() + " - PUT /medical-attention-rates/" + province);
+
+        if (province != medicalAttentionRate.province || year != medicalAttentionRate.year) {
+            res.sendStatus(409);
+            console.warn(Date() + "Hacking attempt!");
+            return;
+        }
+
+
+        db.update({ "province": medicalAttentionRate.province, "year": medicalAttentionRate.year }, medicalAttentionRate, (err, numUpdated) => {
+            console.log("Updated: " + numUpdated);
+        });
+
         res.sendStatus(200);
     });
 
