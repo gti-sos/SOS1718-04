f5c135ecbbd95b92fd4356b9a7b4eacbedce4eea
sos171804crp <crirompov@alum.us.es>
2018-04-21 19:44:25 +0000

    Listo los datos de mi API #25

---
+++ public/unemploymentRatesListCtrl.js
@@ -0,0 +1,45 @@
+ /*global angular*/
+angular.module("UnemploymentRatesApp")
+  .controller("unemploymentRatesListCtrl", ["$scope","$http", function($scope,$http) {
+            console.log("List Ctrl initialized!");
+            var api = "/api/v1/unemployment-rates";
+             
+             $scope.addUnemploymentRate= function(){
+                $http.post(api,$scope.newUnemploymentRate).then(function (response){
+                    $scope.status= "Status: "+ response.status;
+                    getUnemploymentRates();
+                },function(){
+                    if($scope.length!=5){
+                    $scope.status="Error 400: No están todos los campos rellenados"
+                    }else{
+                    $scope.status="Error 409: Ya existe la tasa de desempleo especificada"
+                    }
+            });
+            }
+             $scope.deleteUnemploymentRate= function(province, year){
+                console.log("Unemployment rate to be delete :" + province,year);
+                $http.delete(api+"/"+province+"/"+year).then(function (response){
+                    $scope.status= "Status: "+ response.status;
+                    getUnemploymentRates();
+                });
+                 getUnemploymentRates();
+            }
+            $scope.deleteStadistics= function(){
+                console.log("all unemployment rates will be delete" );
+                $http.delete(api+"/").then(function (response){
+                    $scope.status= "Status: "+ response.status;
+                    getUnemploymentRates();
+                });
+                 getUnemploymentRates();
+            }
+      
+            function getUnemploymentRates(){
+                $http.get(api).then(function (response){
+                    $scope.unemploymentRates = response.data;
+                });
+            }
+           
+            
+            getUnemploymentRates();
+            
+        }]);
---
+++ public/unemploymentRatesManagerApp.js
@@ -0,0 +1,14 @@
+/*global angular*/
+angular
+    .module("UnemploymentRatesApp", ["ngRoute"])
+        .config(function($routeProvider){
+            $routeProvider
+                .when("/",{
+                    templateUrl:"unemploymentRatesList.html",
+                    controller: "unemploymentRatesListCtrl"
+                })
+                .when("/unemploymentRates/:province/:year",{
+                    templateUrl:"unemploymentRatesEdit.html",
+                    controller: "unemploymentRatesEditCtrl"
+                });
+    });
