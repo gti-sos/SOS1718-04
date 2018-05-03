712f830083ac562101b226eb2a8d462f228fd016
sos171804arg <rgfandres@gmail.com>
2018-05-03 14:12:08 +0000

    visualizaciones

--- test/config.js
+++ test/config.js
@@ -1,5 +1,5 @@
 exports.config= {
-    seleniumAddress: 'https://sos1718-04.herokuapp.com/',
+    seleniumAddress: 'https://sos1718-04.herokuapp.com/RoRoMonApp.html#!/graduation-rates',
     specs:['t01-loadDataGraduationRates.js','t02-addStadisticGraduationRates.js'],
     capabilities :{
         'browserName': 'phantomjs'
