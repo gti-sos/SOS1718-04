ac299343365ccbc63f6165d2f30eed3742a9daf3
Carlos <carlos.jmt93@gmail.com>
2018-05-10 20:59:37 +0000

    grafica sin proxi

--- public/sharedApisMedicalAttentionRatesCtrl.js
+++ public/sharedApisMedicalAttentionRatesCtrl.js
@@ -8,7 +8,7 @@ angular.module("RoRoMonApp")
     .controller("sharedApisMedicalAttentionRatesCtrl", ["$scope", "$http", function($scope, $http) {
         console.log("Vistas Ctrl initialized!");
         var ownApi = "/api/v1/medical-attention-rates";
-        var externalApi = "";
+        var externalApi = "https://sos1718-12.herokuapp.com/api/v2/rape-stats";
         var proxy = "proxyCAC/api/v2/crimes-an";
 
         $http
@@ -90,14 +90,87 @@ angular.module("RoRoMonApp")
 
             });
 
-
+        //Esperando que tenga cors
         $http
             .get(externalApi)
             .then(function(auxResponse) {
                 $http
                     .get(ownApi)
                     .then(function(response) {
-
+                        Highcharts.chart('sharedStadistics2', {
+                            chart: {
+                                type: 'column'
+                            },
+                            title: {
+                                text: 'Efficiency Optimization by Branch'
+                            },
+                            xAxis: {
+                                categories: [
+                                    'Seattle HQ',
+                                    'San Francisco',
+                                    'Tokyo'
+                                ]
+                            },
+                            yAxis: [{
+                                min: 0,
+                                title: {
+                                    text: 'Employees'
+                                }
+                            }, {
+                                title: {
+                                    text: 'Profit (millions)'
+                                },
+                                opposite: true
+                            }],
+                            legend: {
+                                shadow: false
+                            },
+                            tooltip: {
+                                shared: true
+                            },
+                            plotOptions: {
+                                column: {
+                                    grouping: false,
+                                    shadow: false,
+                                    borderWidth: 0
+                                }
+                            },
+                            series: [{
+                                name: 'Employees',
+                                color: 'rgba(165,170,217,1)',
+                                data: [150, 73, 20],
+                                pointPadding: 0.3,
+                                pointPlacement: -0.2
+                            }, {
+                                name: 'Employees Optimized',
+                                color: 'rgba(126,86,134,.9)',
+                                data: [140, 90, 40],
+                                pointPadding: 0.4,
+                                pointPlacement: -0.2
+                            }, {
+                                name: 'Profit',
+                                color: 'rgba(248,161,63,1)',
+                                data: [183.6, 178.8, 198.5],
+                                tooltip: {
+                                    valuePrefix: '$',
+                                    valueSuffix: ' M'
+                                },
+                                pointPadding: 0.3,
+                                pointPlacement: 0.2,
+                                yAxis: 1
+                            }, {
+                                name: 'Profit Optimized',
+                                color: 'rgba(186,60,61,.9)',
+                                data: [203.6, 198.8, 208.5],
+                                tooltip: {
+                                    valuePrefix: '$',
+                                    valueSuffix: ' M'
+                                },
+                                pointPadding: 0.4,
+                                pointPlacement: 0.2,
+                                yAxis: 1
+                            }]
+                        });
 
 
 
