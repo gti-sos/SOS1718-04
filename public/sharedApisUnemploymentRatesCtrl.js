b1364af0435da9b7baa4807389a20aa5de76264e
sos171804crp <crirompov@alum.us.es>
2018-05-12 00:42:52 +0000

    Limpio un poco el código del js de la vista compartida y también modifico el dato del spline para ver si puedo hacer una especie de media aunque en este caso sea solo una suma

--- public/sharedApisUnemploymentRatesCtrl.js
+++ public/sharedApisUnemploymentRatesCtrl.js
@@ -9,77 +9,7 @@ angular.module("RoRoMonApp")
             var apiPropia = "/api/v1/unemployment-rates"
             var api1 = "proxySA/api/v2/students-an";
             var api2 = "https://sos1718-02.herokuapp.com/api/v2/employments/";
-/*
-            $http.get(api1).then(function(response1){
-                $http.get(apiPropia).then(function(response2){
-              Highcharts.chart('sharedStadistics1', {
-    chart: {
-        type: 'spline'
-    },
-    title: {
-        text: 'Mixed Stats1'
-    },
-    
-    xAxis: {
-        categories: response2.data.map(function(d){return (parseInt(d.year))})
-    },
-    yAxis: {
-        title: {
-            text: 'Mixed Stats'
-        },
-        labels: {
-            formatter: function () {
-                return this.value ;
-            }
-        }
-    },
-    tooltip: {
-        crosshairs: true,
-        shared: true
-    },
-    plotOptions: {
-        spline: {
-            marker: {
-                radius: 4,
-                lineColor: '#666666',
-                lineWidth: 1
-            }
-        }
-    },
-    series: [{
-        name: 'Illiterate',
-        data: response2.data.map(function(d){return parseFloat(d["illiterate"])})
 
-    },{name: 'First grade',
-        data: response2.data.map(function(d){return parseFloat(d["first-grade"])})
-
-    },{name: 'Second grade',
-        data: response2.data.map(function(d){return parseFloat(d["second-grade"])})
-        
-    }, {
-        name: 'Third degree',
-        data: response2.data.map(function(d){return parseFloat(d["third-degree"])})
-        
-    },{
-        name: 'Pop Illiterate',
-        data: response1.data.map(function(d){return parseFloat(d["popilliterate"])})
-
-    },{
-        name: 'Pop High Education',
-        data: response1.data.map(function(d){return parseFloat(d["pophigheducation"])})
-        
-       
-    },{
-        name: 'Pop In University',
-        data: response1.data.map(function(d){return parseFloat(d["popinuniversity"])})
-        
-       
-    }]
-});
-});
-        });
-
-*/
             $http.get(api1).then(function(response1){
                 $http.get(apiPropia).then(function(response2){
                     
@@ -153,6 +83,8 @@ Highcharts.chart('sharedStadistics1', {
         
         $http.get(api2).then(function(response1){
                 $http.get(apiPropia).then(function(response2){
+                var dato1 = 0.0;
+                var dato2 = 0.0;
                     
 Highcharts.chart('sharedStadistics2', {
     title: {
@@ -178,11 +110,11 @@ Highcharts.chart('sharedStadistics2', {
     }, {
         type: 'column',
         name: 'totalcontributingfamilyworker',
-        data: response1.data.map(function(d){return parseFloat(d["totalcontributingfamilyworker"])})
+        data: response1.data.map(function(d){dato1 = parseFloat(d["totalcontributingfamilyworker"]); return dato1;})
     }, {
         type: 'spline',
         name: 'illiterate-aux',
-        data: response2.data.map(function(d){return parseFloat(d["illiterate"])}),
+        data: response2.data.map(function(d){dato2 = parseFloat(d["illiterate"]); return dato2}),
         marker: {
             lineWidth: 2,
             lineColor: Highcharts.getOptions().colors[3],
@@ -190,72 +122,6 @@ Highcharts.chart('sharedStadistics2', {
         }
     }, ]
 });
-
-                    /*
-              Highcharts.chart('sharedStadistics2', {
-    chart: {
-        type: 'bar'
-    },
-    title: {
-        text: 'Shared stadistic2'
-    },
-    
-    xAxis: {
-        categories: response2.data.map(function(d){return (parseInt(d.year))}),
-        title: {
-            text: null
-        }
-    },
-    yAxis: {
-        min: 0,
-        title: {
-           
-            align: 'high'
-        },
-        labels: {
-            overflow: 'justify'
-        }
-    },
-    
-    plotOptions: {
-        bar: {
-            dataLabels: {
-                enabled: true
-            }
-        }
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'top',
-        x:40,
-        y: 80,
-        floating: true,
-        borderWidth: 1,
-        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-        shadow: true
-    },
-    credits: {
-        enabled: false
-    },
-/*
-    {
-        name: 'Illiterate',
-        type: 'column',
-        yAxis: 1,
-        data: response2.data.map(function(d){return parseFloat(d["illiterate"])})
-
-    }
-
-    series: [{
-        name: 'Illiterate',
-        data: response2.data.map(function(d){return parseFloat(d["illiterate"])})
-    },{
-        name: 'Illiterate',
-         data: response2.data.map(function(d){return parseFloat(d["illiterate"])})
-     }]
- });
- */
          });
         });
            
