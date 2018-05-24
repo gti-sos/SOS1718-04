a5e48a0ea3bb3272ef99374f54e9b38dde6893aa
sos171804arg <rgfandres@gmail.com>
2018-05-24 11:17:34 +0000

    nuevas visualizaciones para las APIS externas

--- public/sharedApisGraduationRatesCtrl.js
+++ public/sharedApisGraduationRatesCtrl.js
@@ -11,6 +11,16 @@ angular.module("RoRoMonApp")
             var api2 = "https://sos1718-05.herokuapp.com/api/v1/country-stats"
             
             
+            
+            
+            
+             //======================================================================
+          //====================================================================== 
+          //==================================DE SOS====================================
+           //======================================================================
+            //======================================================================
+            
+            
             $http.get(api1).then(function(response1){
                 $http.get(apiPropia).then(function(response2){
               Highcharts.chart('sharedStadistics1', {
@@ -76,6 +86,10 @@ angular.module("RoRoMonApp")
         });
         
         
+        //======================================================================
+         //======================================================================
+        
+        
         
          $http.get(api2).then(function(response1){
                 $http.get(apiPropia).then(function(response2){
@@ -135,5 +149,87 @@ angular.module("RoRoMonApp")
 });
         });
         });
+        
+        
+        
+        
+        
+         //======================================================================
+          //====================================================================== 
+          //==================================EXTERNAS====================================
+           //======================================================================
+            //======================================================================
+        
+        
+        $http.get(apiPropia).then(function(response2){
+        
+                anychart.onDocumentReady(function () {
+    // create pie chart with passed data
+    var chart = anychart.pie([
+        
+        ['Public S',response2.data.map(function(d){return (parseFloat(d["public-school"] ))}).reduce(function (previous, current) {
+        return previous + current;})/response2.data.filter(d=>d["public-school"]).length],
+        ['Private S',response2.data.map(function(d){return (parseFloat(d["private-school"] ))}).reduce(function (previous, current) {
+        return previous + current;})/response2.data.filter(d=>d["private-school"]).length],
+        ['Charter S',response2.data.map(function(d){return (parseFloat(d["charter-school"] ))}).reduce(function (previous, current) {
+        return previous + current;})/response2.data.filter(d=>d["charter-school"]).length],
+        
+    ]);
+
+    // set chart title text settings
+    chart.title('Media de aprobados dependiendo del tipo de colegio')
+            //set chart radius
+            .radius('43%')
+            // create empty area in pie chart
+            .innerRadius('30%');
+
+    // set container id for the chart
+    chart.container("sharedStadistics3");
+    // initiate chart drawing
+    chart.draw();
+});
+                
+        });
+        
+        
+        
+         $http.get(apiPropia).then(function(response2){
+        
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+         ['year', 'media'],
+         [2014, parseInt(response2.data.filter(d=>d.year==2014)
+                    .map(function(d){return (parseFloat(d["private-school"]+d["public-school"]+d["charter-school"] ))}).reduce(function (previous, current) {
+                    return previous + current;})/response2.data.filter(d=>d["private-school"]).length)], 
+        [ 2015, parseInt(response2.data.filter(d=>d.year==2015)
+                    .map(function(d){return (parseFloat(d["private-school"]+d["public-school"]+d["charter-school"] ))}).reduce(function (previous, current) {
+                    return previous + current;})/response2.data.filter(d=>d["private-school"]).length)], 
+        [ 2016, parseInt(response2.data.filter(d=>d.year==2016)
+                    .map(function(d){return (parseFloat(d["private-school"]+d["public-school"]+d["charter-school"] ))}).reduce(function (previous, current) {
+                    return previous + current;})/response2.data.filter(d=>d["private-school"]).length)],
+        [ 2017, parseInt(response2.data.filter(d=>d.year==2017)
+                    .map(function(d){return (parseFloat(d["private-school"]+d["public-school"]+d["charter-school"] ))}).reduce(function (previous, current) {
+                    return previous + current;})/response2.data.filter(d=>d["private-school"]).length)]
+            
+        ]);
+
+        var options = {
+          title: 'year vs. media',
+          vAxis: {title: 'media', minValue: 0, 
+                                maxValue: 100},
+          hAxis: {title: 'year', minValue: response2.data.map(function(d){return Math.min(parseInt(d.year ))})
+                                 , maxValue: response2.data.map(function(d){return Math.max(parseInt(d.year ))})},
+          legend: 'none'
+        };
+
+        var chart = new google.visualization.ScatterChart(document.getElementById('sharedStadistics4'));
+
+        chart.draw(data, options);
+      }
+   
+         });
            
 }]);
