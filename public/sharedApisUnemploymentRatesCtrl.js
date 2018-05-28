59d8a81e19bbd19ebbeaae6e3b1cae6c42c203b2
sos171804crp <crirompov@alum.us.es>
2018-05-28 12:26:44 +0000

    Cambio la primera gráfica de mis integraciones para que todas sean diferentes

--- public/sharedApisUnemploymentRatesCtrl.js
+++ public/sharedApisUnemploymentRatesCtrl.js
@@ -41,6 +41,128 @@ angular.module("RoRoMonApp")
             $http.get(api1).then(function(response1){
                 $http.get(apiPropia).then(function(response2){
                     
+                    // Give the points a 3D feel by adding a radial gradient
+Highcharts.setOptions({
+    colors: $.map(Highcharts.getOptions().colors, function (color) {
+        return {
+            radialGradient: {
+                cx: 0.4,
+                cy: 0.3,
+                r: 0.5
+            },
+            stops: [
+                [0, color],
+                [1, Highcharts.Color(color).brighten(-0.2).get('rgb')]
+            ]
+        };
+    })
+});
+
+// Set up the chart
+var chart = new Highcharts.Chart({
+    chart: {
+        renderTo: 'sharedStadistics1',
+        margin: 100,
+        type: 'scatter3d',
+        animation: false,
+        options3d: {
+            enabled: true,
+            alpha: 10,
+            beta: 30,
+            depth: 250,
+            viewDistance: 5,
+            fitToPlot: false,
+            frame: {
+                bottom: { size: 1, color: 'rgba(0,0,0,0.02)' },
+                back: { size: 1, color: 'rgba(0,0,0,0.04)' },
+                side: { size: 1, color: 'rgba(0,0,0,0.06)' }
+            }
+        }
+    },
+    title: {
+        text: 'Illiterate VS Pop Illiterate'
+    },
+    subtitle: {
+        text: 'Click and drag the plot area to rotate in space'
+    },
+    plotOptions: {
+        scatter: {
+            width: 10,
+            height: 10,
+            depth: 10
+        }
+    },
+    yAxis: {
+        min: 0,
+        max: 10,
+        title: null
+    },
+    xAxis: {
+        min: 0,
+        max: 10,
+        gridLineWidth: 1
+    },
+    zAxis: {
+        min: 0,
+        max: 10,
+        showFirstLabel: false
+    },
+    legend: {
+        enabled: false
+    },
+    series: [{
+        name: 'Illiterate',
+        type: 'column',
+        yAxis: 1,
+        data: response2.data.map(function(d){return parseFloat(d["illiterate"])})
+
+    },{
+        name: 'Pop Illiterate',
+        type: 'spline',
+        data: response1.data.map(function(d){return parseFloat(d["popilliterate"])})
+
+    }]
+});
+
+
+// Add mouse and touch events for rotation
+(function (H) {
+    function dragStart(eStart) {
+        eStart = chart.pointer.normalize(eStart);
+
+        var posX = eStart.chartX,
+            posY = eStart.chartY,
+            alpha = chart.options.chart.options3d.alpha,
+            beta = chart.options.chart.options3d.beta,
+            sensitivity = 5; // lower is more sensitive
+
+        function drag(e) {
+            // Get e.chartX and e.chartY
+            e = chart.pointer.normalize(e);
+
+            chart.update({
+                chart: {
+                    options3d: {
+                        alpha: alpha + (e.chartY - posY) / sensitivity,
+                        beta: beta + (posX - e.chartX) / sensitivity
+                    }
+                }
+            }, undefined, undefined, false);
+        }
+
+        chart.unbindDragMouse = H.addEvent(document, 'mousemove', drag);
+        chart.unbindDragTouch = H.addEvent(document, 'touchmove', drag);
+
+        H.addEvent(document, 'mouseup', chart.unbindDragMouse);
+        H.addEvent(document, 'touchend', chart.unbindDragTouch);
+    }
+    H.addEvent(chart.container, 'mousedown', dragStart);
+    H.addEvent(chart.container, 'touchstart', dragStart);
+}(Highcharts));
+
+
+                    ////////**********
+/*
 Highcharts.chart('sharedStadistics1', {
     chart: {
         zoomType: 'xy'
@@ -106,6 +228,7 @@ Highcharts.chart('sharedStadistics1', {
 
     }]
 });
+*/
                 });
             });
         
