60133f4bf1aeef0359ae5456333e0d8681155686
Carlos <carlos.jmt93@gmail.com>
2018-06-01 22:50:09 +0000

    int

--- public/sharedApisMedicalAttentionRatesCtrl.js
+++ public/sharedApisMedicalAttentionRatesCtrl.js
@@ -273,7 +273,7 @@ angular.module("RoRoMonApp")
 
 
 
-
+                                                                                /*
                                                                                 console.log('ejemplo1'+response1.data.filter(d => d.province === 'sevilla' && d.year===2016 ).map(function(d) { return d["general-medicine"] })[0]);
                                                                                 console.log('ejemplo2'+response1.data.filter(d => d.province === 'sevilla' && d.year===2016 )['general-medicine']);
                                                                                 
@@ -321,7 +321,45 @@ angular.module("RoRoMonApp")
                                                                                         ]
                                                                                     }]
                                                                                 });
+                                                                            */
 
+
+
+
+                                                                                Highcharts.chart('sharedStadistics3', {
+                                                                                    chart: {
+                                                                                        type: 'line'
+                                                                                    },
+                                                                                    title: {
+                                                                                        text: 'Monthly Average Temperature'
+                                                                                    },
+                                                                                    subtitle: {
+                                                                                        text: 'Source: WorldClimate.com'
+                                                                                    },
+                                                                                    xAxis: {
+                                                                                        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                                                                                    },
+                                                                                    yAxis: {
+                                                                                        title: {
+                                                                                            text: 'Temperature (°C)'
+                                                                                        }
+                                                                                    },
+                                                                                    plotOptions: {
+                                                                                        line: {
+                                                                                            dataLabels: {
+                                                                                                enabled: true
+                                                                                            },
+                                                                                            enableMouseTracking: false
+                                                                                        }
+                                                                                    },
+                                                                                    series: [{
+                                                                                        name: 'Tokyo',
+                                                                                        data: [7.0, 6.9, 9.5, 14.5, 18.4, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+                                                                                    }, {
+                                                                                        name: 'London',
+                                                                                        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+                                                                                    }]
+                                                                                });
                                                                             });
                                                                     });
                                                             });
