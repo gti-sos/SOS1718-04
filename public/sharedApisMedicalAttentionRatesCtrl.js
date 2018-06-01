ad2400538cdbd078c6ef8bed53e310497b51bbbc
Carlos <carlos.jmt93@gmail.com>
2018-06-01 22:30:44 +0000

    int

--- public/sharedApisMedicalAttentionRatesCtrl.js
+++ public/sharedApisMedicalAttentionRatesCtrl.js
@@ -274,8 +274,8 @@ angular.module("RoRoMonApp")
 
 
 
-                                                                                console.log('ejemplo1'+response1.data.filter(d => d.province === 'sevilla' && d.year===2014 ).map(function(d) { return d["general-medicine"] })[0]);
-                                                                                console.log('ejemplo2'+response1.data.filter(d => d.province === 'sevilla' && d.year===2014 )['general-medicine']);
+                                                                                console.log('ejemplo1'+response1.data.filter(d => d.province === 'sevilla' && d.year===2016 ).map(function(d) { return d["general-medicine"] })[0]);
+                                                                                console.log('ejemplo2'+response1.data.filter(d => d.province === 'sevilla' && d.year===2016 )['general-medicine']);
                                                                                 
                                                                                 console.log(response2.data.length);
                                                                                 console.log(response2.data.Results[0]);
